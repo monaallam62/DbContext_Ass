7f0baf56fef958a8908b760ddde7b556d4cca0d4
monaallam62 <monaallam5102@gmail.com>
2026-04-22 20:41:03 +0200

    DbContext

---
+++ DbContext_Ass/DBContext/AddDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DbContext_Ass.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbContext_Ass.DBContext
+{
+    public class AddDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=DbBOOKSTORE;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<BookInfoo> Books { get; set; }
+        public DbSet<AuthorsInfo> AuthorsInfo { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+    }
+}
