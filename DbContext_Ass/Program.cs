9192fc29a9eec6f20a5b2f29ead113520189f01c
monaallam62 <monaallam5102@gmail.com>
2026-04-22 19:48:54 +0200

    Add project files.

---
+++ DbContext_Ass/Program.cs
@@ -0,0 +1,10 @@
+﻿ namespace DbContext_Ass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
